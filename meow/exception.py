ae9cf8c2df1e8a739657cf7b1c431c1f64312a43
Simon Kennedy <sffjunkie@yahoo.co.uk>
2008-10-21 17:08:03 +0100

    Initial Commit

---
+++ meow/exception.py
@@ -0,0 +1,5 @@
+__all__ = ['HandlerError']
+
+class HandlerError(Exception):
+    pass
+
---
+++ meow/growl.py
@@ -0,0 +1,7 @@
+class Growler(object):
+    def __init__(self):
+        pass
+
+    def sendMessage(self, message, timeout=0):
+        pass
+
