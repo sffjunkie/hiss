688d1d7561827db4d9c02e97c5b81a36c4e3583e
Simon Kennedy <sffjunkie@yahoo.co.uk>
2008-10-22 20:42:54 +0100

    Moved code from hiss.py to __init__.py

--- hiss/__init__.py
+++ hiss/__init__.py
@@ -1,1 +1,195 @@
+# Copyright 2008, Simon Kennedy, sdk@sffjunkie.co.uk.
+# Distributed under the terms of the MIT License.
+
+# The Python Snarl and Growl bindings
+
+import os
+
+import snarler
+import growler
+
+from exception import *
+
+__all__ = ['Message', 'Application', 'Sid']
+
+class Message(object):
+    def __init__(self):
+        self._id = 0
+        self._title = ''
+        self._text = ''
+        self._icon = ''
+        self._sound = ''
+        self._timeout = 0
+
+        self._handler = None
+
+    def _get_id(self):
+        return self._id
+
+    def _set_id(self, id):
+        self._id = id
+
+    ID = property(_get_id, _set_id)
+
+    def _get_title(self):
+        return self._title
+
+    def _set_title(self, title):
+        if len(title) > 1024:
+            raise ValueError('Maximum title length (1024) exceeded')
+
+        self._title = title
+
+    Title = property(_get_title, _set_title)
+
+    def _get_text(self):
+        return self._text
+
+    def _set_text(self, text):
+        self._text = text
+
+    Text = property(_get_text, _set_text)
+
+    def _get_icon(self):
+        return self._icon
+
+    def _set_icon(self, icon):
+        self._icon = icon
+
+    Icon = property(_get_icon, _set_icon)
+
+    def _get_sound(self):
+        return self._sound
+
+    def _set_sound(self, sound):
+        self._sound = sound
+
+    Sound = property(_get_sound, _set_sound)
+
+    def _get_handler(self):
+        return self._handler
+
+    def _set_handler(self, handler):
+        self._handler = handler
+
+    Handler = property(_get_handler, _set_handler)
+
+    def _get_timeout(self):
+        return self._timeout
+
+    def _set_timeout(self, timeout):
+        self._timeout = timeout
+
+        if self._handler is not None and self._handler != 0:
+            self._handler.setTimeout(self)
+
+    Timeout = property(_get_timeout, _set_timeout)
+
+    def Show(self, timeout=0):
+        if self._handler is None:
+            raise HandlerError('No handler set.')
+
+        self._handler.showMessage(self)
+
+    def Hide(self):
+        if self._handler is None:
+            raise HandlerError('No handler set.')
+
+        self._handler.hideMessage(self)
+
+    IsValid = property(lambda self: self._title != '' and self._text != '')
+
+    def _get_is_visible(self):
+        if self._handler is None:
+            raise HandlerError('No handler set.')
+
+        return self._handler.msgIsVisible(self)
+
+    IsVisible = property(_get_is_visible)
+
+
+class Application(object):
+    def __init__(self):
+        self._handle = 0
+        self._title = ''
+        self._icon = ''
+        self._alerts = []
+
+    def _get_handle(self):
+        return self._handle
+
+    def _set_handle(self, handle):
+        self._handle = handle
+
+    Handle = property(_get_handle, _set_handle)
+
+    def _get_title(self):
+        return self._title
+
+    def _set_title(self, title):
+        self._title = title
+
+    Title = property(_get_title, _set_title)
+
+    def _get_icon(self):
+        return self._icon
+
+    def _set_icon(self, icon):
+        self._icon = icon
+
+    Icon = property(_get_icon, _set_icon)
+
+    def RegisterAlert(self, title, handler):
+        if title not in self._alerts:
+            handler.registerAlert(self._title, title)
+            self._alerts.append(title)
+
+    def RevokeAlert(self, title, handler):
+        if title in self._alerts:
+            handler.revokeAlert(self._title, title)
+            self._alerts.remove(title)
+
+
+class Sid(object):
+    def __init__(self, handler=None):
+        if handler is None:
+            if os.name == 'osx':
+                handler = 'growl'
+            else:
+                handler = 'snarl'
+
+        if handler == 'snarl':
+            self._handler = snarler.Snarler()
+        elif handler == 'growl':
+            self._handler = growler.Growler()
+        else:
+            raise ValueError('Unknown handler %s specified' % handler)
+
+        self._handler_name = handler
+
+    Handler = property(lambda self: self._handler)
+    HandlerName = property(lambda self: self._handler.Name)
+    HandlerVersion = property(lambda self: self._handler.Version)
+    HandlerPath = property(lambda self: self._handler.getAppPath())
+    IconPath = property(lambda self: self._handler.getIconsPath())
+
+    def ShowMessage(self, msg, timeout=0):
+        msg.Handler = self._handler
+        msg.Timeout = timeout
+        msg.Show()
+
+    def HideMessage(self, msg):
+        msg.Hide()
+
+    def RegisterApp(self, app):
+        self._handler.registerConfig(app)
+
+    def RevokeApp(self, app):
+        self._handler.revokeConfig(app)
+
+    def RegisterAlert(self, app, title):
+        app.RegisterAlert(title, self._handler)
+
+    def RevokeAlert(self, app, title):
+        app.RevokeAlert(title, self._handler)
 
