1bd7f40bc8fe44604dbf26dd15a792a0794884e6
Simon Kennedy <sffjunkie@yahoo.co.uk>
2008-10-22 20:41:14 +0100

    Added distutils setup.py file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='hiss',
+      version='0.0.1',
+      description='hiss is a Python interface to the Snarl application.',
+      author='Simon Kennedy',
+      license='MIT',
+      packages=['hiss'],
+      )
+
