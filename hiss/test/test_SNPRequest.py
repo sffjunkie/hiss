8a518ea1b72349a59bc199ce9a480ef07c9e0652
sffjunkie <code@sffjunkie.co.uk>
2014-02-06 12:40:08 +0000

    * Renamed test functions * Removed __main__ test and test method calling

--- hiss/test/test_SNPRequest.py
+++ hiss/test/test_SNPRequest.py
@@ -15,7 +15,7 @@
 from hiss.hash import HashInfo
 from hiss.handler.snp import Request
 
-def test_Request_Create():
+def test_SNP_Request_Create():
 	m = Request()
 
 def test_Request_Append():
@@ -36,21 +36,21 @@ def test_Request_Append():
 	m.append('add_class', name='sig')
 	assert len(m.commands) == 3
 
-def test_Request_Marshall2():
+def test_SNP_Request_Marshall2():
 	m = Request(version='2.0')
 	m.append('register', signature='application/x-vnd-sffjunkie.hiss', uid='1', title='ww')
 
 	cmd = m.marshall()
 	assert cmd == b'snp://register?signature=application/x-vnd-sffjunkie.hiss&title=ww&uid=1\r\n'
 
-def test_Request_Marshall3():
+def test_SNP_Request_Marshall3():
 	m = Request(version='3.0')
 	m.append('register', signature='application/x-vnd-sffjunkie.hiss', uid='1', title='ww')
 
 	cmd = m.marshall()
 	assert cmd == b'SNP/3.0\r\nregister?signature=application/x-vnd-sffjunkie.hiss&title=ww&uid=1\r\nEND\r\n'
 
-def test_Request_Unmarshall2():
+def test_SNP_Request_Unmarshall2():
 	cmd = b'snp://register?signature=x-vnd-sffjunkie.hiss&title=ww&uid=1\r\n'
 	request = Request()
 	request.unmarshall(cmd)
@@ -60,7 +60,7 @@ def test_Request_Unmarshall2():
 	assert len(request.commands[0][1]) == 3
 	assert len(request.commands[0].parameters) == 3
 
-def test_Request_Unmarshall3():
+def test_SNP_Request_Unmarshall3():
 	cmd = (b'SNP/3.0 NONE MD5:b7c903901cab976ee5db15792eb15a03.1A2B3C4D5E6F\r\n'
 		   b'register?signature=x-vnd-sffjunkie.hiss&title=ww&uid=1\r\n'
 		   b'add_class?signature=x-vnd-sffjunkie.hiss\r\n'
@@ -77,11 +77,3 @@ def test_Request_Unmarshall3():
 	assert request._hash == HashInfo('md5',
 						    'b7c903901cab976ee5db15792eb15a03',
 						    '1A2B3C4D5E6F')
-
-if __name__ == '__main__':
-	test_Request_Create()
-	test_Request_Append()
-	test_Request_Marshall2()
-	test_Request_Marshall3()
-	test_Request_Unmarshall2()
-	test_Request_Unmarshall3()
