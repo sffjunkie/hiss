4eac04083cd07d3e6f9e823ae36668db074239b4
sffjunkie <code@sffjunkie.co.uk>
2014-02-06 14:37:53 +0000

    EncryptionInfo tests added

---
+++ hiss/test/test_EncryptionInfo.py
@@ -0,0 +1,31 @@
+import pytest
+
+from hiss.encryption import EncryptionInfo
+
+def test_EncryptionInfo_Init_BadInit():
+    with pytest.raises(TypeError):
+        _ei = EncryptionInfo()
+
+def test_EncryptionInfo_Init_GoodInit():
+    _ei = EncryptionInfo('AES', b'1111111111')
+
+def test_EncryptionInfo_Init_BadAlgorithm():
+    with pytest.raises(ValueError):
+        _ei = EncryptionInfo('WALLY', b'1111111111')
+
+def test_EncryptionInfo_Init_Algorithm_IsUpperCase():
+    ei = EncryptionInfo('AES', b'1111111111')
+    assert ei.algorithm == 'AES'
+
+def test_EncryptionInfo_Init_Algorithm_AnyCase():
+    ei = EncryptionInfo('aEs', b'1111111111')
+    assert ei.algorithm == 'AES'
+
+def test_EncryptionInfo_Init_ivBytes():
+    ei = EncryptionInfo('aEs', b'1111111111')
+    assert ei.iv == b'1111111111'
+
+def test_EncryptionInfo_Init_ivStr():
+    ei = EncryptionInfo('aEs', '1111111111')
+    assert ei.iv == b'\x11\x11\x11\x11\x11'
+    
