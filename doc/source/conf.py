c64fe41694e77d44fec288286d28c49efee9e024
sffjunkie <code@sffjunkie.co.uk>
2014-02-24 16:01:25 +0000

    Removed theme options as they are not required

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -117,7 +117,7 @@ else:
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'project_logo': 'snake_notification.png'}
+#html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [theme_root]
